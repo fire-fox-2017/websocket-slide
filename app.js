9c79a83a55a8ba61ba5ed97ae7d2f122c4025b4d
Bhirawa Mbani <bhirmbani@gmail.com>
2017-05-10 22:05:10 +0700

    done

---
+++ app.js
@@ -0,0 +1,10 @@
+var firebase = require('firebase');
+
+var config = {
+  apiKey: "AIzaSyDcxulhYtfskdf0sv7M8H5sVlw0bl-gSgU",
+  authDomain: "slideshow-firefox.firebaseapp.com",
+  databaseURL: "https://slideshow-firefox.firebaseio.com",
+  storageBucket: "slideshow-firefox.appspot.com",
+};
+firebase.initializeApp(config);
+
